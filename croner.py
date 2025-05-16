ebfee95404a2298c3c51a987c3fa252506c45e17
SorokinVN23 <SorokinVN23@gmail.com>
2025-05-16 22:44:02 +0300

    first form PySide

---
+++ croner.py
@@ -0,0 +1,26 @@
+import random 
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from design import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.generate_number)
+
+    def generate_number(self):
+        number = random.randint(1, 100)
+        self.ui.label.setText(str(number))
+
+def main():
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
